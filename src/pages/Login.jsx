f67e125c2f3ce3548a52d81f93c0d456acd4a670
kennion <kennion@byu.edu>
2025-03-31 23:02:47 -0600

    Does this script actually do what it says it does

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -178,7 +178,7 @@ export default function Login({ loginType }) {
 
     if (message == null) {
       try {
-        const response = await fetch("https://bibliotrace-service.bibliotrace.com/api/auth/login", {
+        const response = await fetch("http://localhost:8080/api/auth/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
