8c07f10074f2c3e91f8bfe8bd1d728ff8a42dc7c
Kelly Ko <kellyko2003@gmail.com>
2025-04-16 09:29:14 -0600

    fix sorttable import

--- src/components/useSorttable.jsx
+++ src/components/useSorttable.jsx
@@ -4,7 +4,7 @@ export default function useSorttable() {
   useEffect(() => {
     //adds sorttable script
     const scriptEl = document.createElement("script");
-    scriptEl.src = "./sorttable.js";
+    scriptEl.src = "../sorttable.js";
     scriptEl.async = true;
     document.body.appendChild(scriptEl);
     return () => {
